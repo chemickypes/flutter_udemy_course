4a2ff44e64bb14d20de16e29980b684d9a8dccbf
Angelo Moroni <mor.angelo.mor@gmail.com>
2019-07-09 19:48:02 +0200

    more style to the question

--- quiz_app/lib/question.dart
+++ quiz_app/lib/question.dart
@@ -1,17 +1,22 @@
-
 import 'package:flutter/material.dart';
 
 class QuestionWidget extends StatelessWidget {
-
-
   final String questionText;
-  const QuestionWidget({Key key, @required this.questionText}) : super(key: key);
-
+  const QuestionWidget({Key key, @required this.questionText})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Text(questionText),
+      width: double.infinity,
+      margin: EdgeInsets.all(10),
+      child: Text(
+        questionText,
+        style: TextStyle(
+          fontSize: 28,
+        ),
+        textAlign: TextAlign.center,
+      ),
     );
   }
 }
