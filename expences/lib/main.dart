856c7d930529d09756130dfda01799bdf1272484
Moroni Angelo <angelo.moroni@hype.it>
2019-07-12 18:20:32 +0200

    add intl

--- expences/lib/main.dart
+++ expences/lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:expences/transaction.dart';
 import 'package:flutter/material.dart';
 
+import 'package:intl/intl.dart';
+
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
@@ -48,19 +50,79 @@ class _MyHomePageState extends State<MyHomePage> {
               elevation: 5,
             ),
           ),
+          Card(
+            elevation: 5,
+            child: Container(
+              padding: EdgeInsets.all(16),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: <Widget>[
+                  TextField(
+                    decoration: InputDecoration(labelText: 'Title'),
+                  ),
+                  TextField(
+                    decoration: InputDecoration(labelText: 'Amount'),
+                  ),
+                  Container(
+                    margin: EdgeInsets.only(top: 20),
+                    child: FlatButton(
+                      child: Text('Add Transaction'),
+                      textColor: Colors.purple,
+                      onPressed: () {},
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ),
           Column(
             children: transactions.map((transaction) {
               return Card(
-                child: Row(
-                  children: <Widget>[
-                    Container(
-                      child: Text(transaction.amount.toString()),
-                    ),
-                    Column( children: <Widget>[
-                      Text(transaction.title),
-                      Text(transaction.date.toString()),
-                    ],)
-                  ],
+                child: Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Row(
+                    children: <Widget>[
+                      Container(
+                        margin:
+                            EdgeInsets.symmetric(vertical: 10, horizontal: 16),
+                        decoration: BoxDecoration(
+                          border: Border.all(
+                            color: Colors.purpleAccent,
+                            width: 2,
+                          ),
+                          borderRadius: BorderRadius.all(Radius.circular(6)),
+                        ),
+                        padding: EdgeInsets.all(10),
+                        child: Text(
+                          "\$${transaction.amount}",
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 20,
+                              color: Colors.deepPurple[200]),
+                        ),
+                      ),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          Text(
+                            transaction.title,
+                            textAlign: TextAlign.start,
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 17,
+                            ),
+                          ),
+                          Text(
+                            DateFormat.yMMMMd().format(transaction.date),
+                            style: TextStyle(
+                              fontSize: 12,
+                              color: Colors.blueGrey,
+                            ),
+                          ),
+                        ],
+                      )
+                    ],
+                  ),
                 ),
               );
             }).toList(),
