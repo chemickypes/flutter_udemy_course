70530291946fdf5d41024f78997bb4e9c92ee25a
Moroni Angelo <angelo.moroni@hype.it>
2019-09-24 10:12:53 +0200

    font

--- meals_app/lib/main.dart
+++ meals_app/lib/main.dart
@@ -16,13 +16,13 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.pink,
         accentColor: Colors.amber,
         canvasColor: Color.fromRGBO(255, 254,229, 1),
-        //fontFamily: 'Raleway',
+        fontFamily: 'Raleway',
         textTheme: ThemeData.light().textTheme.copyWith(
           body1: TextStyle(color: Color.fromRGBO(20, 51, 51, 1),),
           body2: TextStyle(color: Color.fromRGBO(20, 51, 51, 1),),
           title: TextStyle(
             fontSize: 20,
-           // fontFamily: 'RobotoCondensed',
+            fontFamily: 'RobotoCondensed',
             fontWeight: FontWeight.bold
           )
         )
